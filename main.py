645259d0e1e205d390d4a26791eaaf0334a4eb86
heyandy889 <heyandy889@users.noreply.github.com>
2015-11-06 15:22:11 -0500

    adding main, demo to list solutions up to a particular value.

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+from cubeint import find_integer_solutions
+
+
+print('')
+print('n^3 = a^3 + b^3 + c^3')
+print('')
+
+print('n' + ': ' + '[a, b, c]')
+for n in range(20):
+	result = find_integer_solutions(n)
+	print(str(n) + ': ' + str(result))
+
