45d489f7489049f98bd4ffa0b6032433e2cba030
heyandy889 <heyandy889@users.noreply.github.com>
2015-11-06 18:10:20 -0500

    it's working!

--- cubeint.py
+++ cubeint.py
@@ -3,39 +3,34 @@ import math
 
 # n = a^3 + b^3 + c3
 
+def cubes(a, b, c):
+	return math.pow(a,3) + math.pow(b,3) + math.pow(c,3)
+
 def find_integer_solutions(n):
 	a = 0
 	b = 0
 	c = 0
 
-	while math.pow(a, 3) < n:
+	while n > cubes(a,b,c):
 		a += 1
 
-	if math.pow(a, 3) > n:
-		a -= 1
+		while n > cubes(a,b,c):
+			b += 1
 
-	while math.pow(b, 3) < (n-math.pow(a,3)):
-		b += 1
+		if n < cubes(a,b,c):
+			b -= 1
 
-	if math.pow(b, 3) > (n-math.pow(a,3)):
-		b -= 1
+		while n > cubes(a,b,c):
+			c += 1
 
-	while math.pow(c, 3) < (n-math.pow(a,3)-math.pow(b,3)):
-		c += 1
+		if n < cubes(a,b,c):
+			b = 0
+			c = 0
 
-	if math.pow(c, 3) > (n-math.pow(a,3)-math.pow(b,3)):
-		print 'uh oh'
+	if n != cubes(a,b,c):
+		return [0, 0, 0]
 
 	return [a, b, c]
-
-
-
-
-
-
-
-
-
 
 
 
--- test.py
+++ test.py
@@ -2,52 +2,32 @@
 from cubeint import find_integer_solutions
 import math
 
+
+def check_equality_order_independent(expected, actual):
+	product_expected = 0
+	product_expected += math.pow(int(expected[0]), 3)
+	product_expected += math.pow(int(expected[1]), 3)
+	product_expected += math.pow(int(expected[2]), 3)
+
+	product_actual = 0
+	product_actual += math.pow(int(actual[0]), 3)
+	product_actual += math.pow(int(actual[1]), 3)
+	product_actual += math.pow(int(actual[2]), 3)
+
+	return product_expected == product_actual
+
 def assertSolution(n, expected, debug=True):
 	actual = find_integer_solutions(n)
 
-	errorstring = 'n = ' + str(n)
-	errorstring += '\nexpected: ' + str(expected)
-	errorstring += '\nactual: ' + str(actual)
-
 	if debug:
-		print 'f(' + str(n) + ') = ' + str(actual)
-	assert actual == expected, errorstring
-
-#assertSolution(n, [a, b, c])
-
-'''
-#known cubes
-assertSolution(0, [0,0,0])
-assertSolution(1, [1,0,0])
-assertSolution(8, [2,0,0])
-assertSolution(970299, [99,0,0])
-
-#double a cubed number
-assertSolution(2, [1, 1, 0])
-assertSolution(16, [2, 2, 0])
-assertSolution(54, [3, 3, 0])
-assertSolution(128, [4, 4, 0])
-assertSolution(1940598, [99,99,0])
-
-#triple a cubed number
-assertSolution(3, [1, 1, 1])
-assertSolution(24, [2, 2, 2])
-assertSolution(81, [3, 3, 3])
-assertSolution(2910897, [99, 99, 99])
-assertSolution(3 * math.pow(10,9), [1000, 1000, 1000])
-assertSolution(3 * math.pow(10,9) + 1, [None, None, None])
-
-#known cases with no solution
-assertSolution(-1, [None, None, None])
-assertSolution(4, [None, None, None])
-assertSolution(5, [None, None, None])
-assertSolution(13, [None, None, None])
-assertSolution(14, [None, None, None])
-assertSolution(22, [None, None, None])
-assertSolution(23, [None, None, None])
-assertSolution(31, [None, None, None])
-assertSolution(32, [None, None, None])
-'''
+		print('f(' + str(n) + ') = ' + str(actual))
+	
+	if not check_equality_order_independent(expected, actual):
+		print('FAIL')
+		print('n = ' + str(n))
+		print('expected: ' + str(expected))
+		print('actual: ' + str(actual))
+		print('')
 
 #just working through simple cases
 assertSolution(0, [0, 0, 0])
@@ -74,3 +54,37 @@ assertSolution(55, [3, 3, 1])
 assertSolution(62, [3, 3, 2])
 assertSolution(81, [3, 3, 3])
 
+assertSolution(-1, [0, 0, 0])
+assertSolution(4,  [0, 0, 0])
+assertSolution(5,  [0, 0, 0])
+assertSolution(13, [0, 0, 0])
+assertSolution(14, [0, 0, 0])
+assertSolution(22, [0, 0, 0])
+assertSolution(23, [0, 0, 0])
+assertSolution(31, [0, 0, 0])
+assertSolution(32, [0, 0, 0])
+
+
+
+'''
+#known cubes
+assertSolution(0, [0,0,0])
+assertSolution(1, [1,0,0])
+assertSolution(8, [2,0,0])
+assertSolution(970299, [99,0,0])
+
+#double a cubed number
+assertSolution(2, [1, 1, 0])
+assertSolution(16, [2, 2, 0])
+assertSolution(54, [3, 3, 0])
+assertSolution(128, [4, 4, 0])
+assertSolution(1940598, [99,99,0])
+
+#triple a cubed number
+assertSolution(3, [1, 1, 1])
+assertSolution(24, [2, 2, 2])
+assertSolution(81, [3, 3, 3])
+assertSolution(2910897, [99, 99, 99])
+assertSolution(3 * math.pow(10,9), [1000, 1000, 1000])
+assertSolution(3 * math.pow(10,9) + 1, [None, None, None])
+'''
