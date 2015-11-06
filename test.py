2646adf0eb99b89ee72c6e28bebf399419f36318
heyandy889 <heyandy889@users.noreply.github.com>
2015-11-06 18:35:17 -0500

    building confidence with more tests

--- test.py
+++ test.py
@@ -64,27 +64,21 @@ assertSolution(23, [0, 0, 0])
 assertSolution(31, [0, 0, 0])
 assertSolution(32, [0, 0, 0])
 
-
-
-'''
-#known cubes
-assertSolution(0, [0,0,0])
-assertSolution(1, [1,0,0])
-assertSolution(8, [2,0,0])
 assertSolution(970299, [99,0,0])
-
-#double a cubed number
-assertSolution(2, [1, 1, 0])
-assertSolution(16, [2, 2, 0])
-assertSolution(54, [3, 3, 0])
-assertSolution(128, [4, 4, 0])
+assertSolution(970696, [66,88,12])
 assertSolution(1940598, [99,99,0])
+assertSolution(736, [6,2,8])
+assertSolution(21429, [23, 1, 21])
+assertSolution(10928, [4, 22, 6])
+assertSolution(9339, [20, 2, 11])
+assertSolution(42552, [24, 12, 30])
+assertSolution(10865, [22, 1, 6])
+assertSolution(15036, [23, 5, 14])
+assertSolution(3555, [3, 13, 11])
+assertSolution(35840, [4, 24, 28])
+assertSolution(198829, [34, 22, 53])
+assertSolution(236762, [36, 17, 57])
+assertSolution(336393, [60, 14, 49])
 
-#triple a cubed number
-assertSolution(3, [1, 1, 1])
-assertSolution(24, [2, 2, 2])
-assertSolution(81, [3, 3, 3])
-assertSolution(2910897, [99, 99, 99])
-assertSolution(3 * math.pow(10,9), [1000, 1000, 1000])
-assertSolution(3 * math.pow(10,9) + 1, [None, None, None])
-'''
+
+
