426d8a53be49749bbaf2b56ff1fb5936ff98c3e5
heyandy889 <heyandy889@users.noreply.github.com>
2015-11-06 16:38:46 -0500

    added known failures, solutions for triple a cube number

--- cubeint.py
+++ cubeint.py
@@ -4,21 +4,23 @@ import math
 # n = a^3 + b^3 + c3
 
 def find_integer_solutions(n):
-	a = 0
-	b = 0
-	for i in range(0,100):
+	a = None
+	b = None
+	c = None
+
+	for i in range(0,1001):
 		if math.pow(i, 3) == n:
 			a = i
-		elif math.pow(i, 3) == n/2:
+			b = 0
+			c = 0
+		elif math.pow(i, 3) == n/2.0:
 			a = i
 			b = i
+			c = 0
+		elif math.pow(i, 3) == n/3.0:
+			a = i
+			b = i
+			c = i
 
-	return [a, b, 0]
-
-
-
-
-
-
-
+	return [a, b, c]
 
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 
 from cubeint import find_integer_solutions
+import math
 
 def assertSolution(n, expected):
 	actual = find_integer_solutions(n)
@@ -8,9 +9,10 @@ def assertSolution(n, expected):
 	errorstring += '\nexpected: ' + str(expected)
 	errorstring += '\nactual: ' + str(actual)
 
-	assert actual ==expected, errorstring
+	assert actual == expected, errorstring
 
 #assertSolution(n, [a, b, c])
+
 assertSolution(0, [0,0,0])
 assertSolution(1, [1,0,0])
 assertSolution(8, [2,0,0])
@@ -21,4 +23,13 @@ assertSolution(16, [2, 2, 0])
 assertSolution(1940598, [99,99,0])
 
 assertSolution(3, [1, 1, 1])
+assertSolution(24, [2, 2, 2])
+assertSolution(81, [3, 3, 3])
+assertSolution(2910897, [99, 99, 99])
+assertSolution(3 * math.pow(10,9), [1000, 1000, 1000])
+assertSolution(3 * math.pow(10,9) + 1, [None, None, None])
+
+assertSolution(-1, [None, None, None])
+assertSolution(25, [None, None, None])
+assertSolution(26, [None, None, None])
 
