c01f25314676819c35275d66624ffd529c2d388d
heyandy889 <heyandy889@users.noreply.github.com>
2015-11-06 15:17:54 -0500

    moving tests to their own file for easier reading and development

--- cubeint.py
+++ cubeint.py
@@ -15,24 +15,6 @@ def find_integer_solutions(n):
 
 	return [a, b, 0]
 
-def assertSolution(n, expected):
-	actual = find_integer_solutions(n)
-
-	errorstring = ''
-	errorstring += '\nexpected: ' + str(expected)
-	errorstring += '\nactual: ' + str(actual)
-
-	assert actual ==expected, errorstring
-
-#assertSolution(n, [a, b, c])
-assertSolution(0, [0,0,0])
-assertSolution(1, [1,0,0])
-assertSolution(8, [2,0,0])
-assertSolution(970299, [99,0,0])
-
-assertSolution(2, [1, 1, 0])
-assertSolution(16, [2, 2, 0])
-assertSolution(1940598, [99,99,0])
 
 
 
---
+++ test.py
@@ -0,0 +1,21 @@
+
+from cubeint import find_integer_solutions
+
+def assertSolution(n, expected):
+	actual = find_integer_solutions(n)
+
+	errorstring = ''
+	errorstring += '\nexpected: ' + str(expected)
+	errorstring += '\nactual: ' + str(actual)
+
+	assert actual ==expected, errorstring
+
+#assertSolution(n, [a, b, c])
+assertSolution(0, [0,0,0])
+assertSolution(1, [1,0,0])
+assertSolution(8, [2,0,0])
+assertSolution(970299, [99,0,0])
+
+assertSolution(2, [1, 1, 0])
+assertSolution(16, [2, 2, 0])
+assertSolution(1940598, [99,99,0])
